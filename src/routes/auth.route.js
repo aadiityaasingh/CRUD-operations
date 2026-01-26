10b4ce67ac039493ab65ae234d2cbd8908288e4e
aadiityaasingh <adityasingh.6488@gmail.com>
2026-01-26 20:51:12 +0530

    added the verify route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const authController = require("../controllers/auth.controller.js");
+const otpController = require("../controllers/otp.controller.js")
 const router = express.Router();
 
 router.post("/user/register", authController.registerUser);
 router.post("/user/login", authController.loginUser);
 router.get("/user/logout", authController.logoutUser);
+
+router.post("/verify-otp", otpController.verifyEmailOtp);
 
 module.exports = router;
